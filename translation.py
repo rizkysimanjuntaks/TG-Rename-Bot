7e50b351cc0bf107e552c787772d94bf10a50db4
rizkysimanjuntaks <91770129+rizkysimanjuntaks@users.noreply.github.com>
2021-10-03 11:06:55 +0700

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,18 +1,18 @@
 class Translation(object):
     START_TEXT = """Hello,
-This is a Telegram Rename Bot!
+Ini adalah SF File Rename Bot!
 
-<b>Please send me any Telegram file and reply to that file to /rename New Name.mkv</b>
+<b>Kirimkan saya file Telegram apa pun dan balas file itu ke /rename Nama Baru.extension</b>
 
-/help for more details.."""
-    RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
-    ABS_TEXT = " Please don't be selfish."
+/help untuk detail lebih lanjut.."""
+    RENAME_403_ERR = "Maaf. Anda tidak diizinkan untuk mengganti nama file ini."
+    ABS_TEXT = " Tolong jangan egois."
     UPGRADE_TEXT = "<b>👉 Create own Clone Bot.. </b>  /help for Details"
-    DOWNLOAD_START = "trying to download"
-    UPLOAD_START = "trying to upload"
-    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.5GB due to Telegram API limitations."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me🤓.**"
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds."
+    DOWNLOAD_START = "Sedang Mengunduh"
+    UPLOAD_START = "Sedang Mengunggah"
+    RCHD_TG_API_LIMIT = "Diunduh dalam {} detik.\nUkuran file terdeteksi : {}\nMaaf. Namun, saya tidak dapat mengunggah file yang lebih besar dari 1,5 GB karena keterbatasan API Telegram."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Terima kasih telah Menggunakan Saya🤓.\n\n~[@SF Corp](htpps://t.me/SFCorpChannel)**"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Diunduh dalam {} detik.\nDiunggah dalam {} detik."
     NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
     NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://telegram.dog/ThankTelegram'>@SpEcHlDe</a>"
     SAVED_CUSTOM_THUMB_NAIL = "Custom File thumbnail saved. This image will be used in the File."
@@ -22,25 +22,23 @@ This is a Telegram Rename Bot!
     CUSTOM_CAPTION_UL_FILE = " "
     NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
-    HELP_USER = """Hai am Rename bot..
+    HELP_USER = """Hai Saya SF File Rename bot..
     
-1. Send me any Telegram File.
-2. Reply to that message to /rename new name.extension.
+1. Kirimi saya File Telegram apa saja.
+2. Balas file Telegram ke /rename Nama Baru File.extension.
    
-<b>👉 Create own Clone Bot :</b> 👉 <a href="https://youtu.be/yYHrSnuYXpA">Diploy</a>
-
---------
-
-Support Group : @InFoTelGroup
-© @SpEcHlDe @TGBotsZ"""
-    REPLY_TO_DOC_FOR_RENAME_FILE = "Reply to a Telegram media to `/rename New Name.extension` with custom thumbnail support.."
+~[@SF Corp](htpps://t.me/SFCorpChannel)
+"""
+    REPLY_TO_DOC_FOR_RENAME_FILE = "Balas file Telegram ke `/rename Nama Baru File.extension`"
     ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction."
     FREE_USER_LIMIT_Q_SZE = """Cannot Process.
 Free users only 1 request per 30 minutes.
 /upgrade or Try 1800 seconds later."""
-    IFLONG_FILE_NAME = """File Name limit allowed by Telegram is {alimit} characters.
-The given file name has {num} characters.
+    IFLONG_FILE_NAME = """Batas Nama File yang diizinkan oleh Telegram adalah {alimit} karakter.
+Nama file yang diberikan memiliki {num} karakter.
 
-<b>Essays Not allowed in Telegram file name!</b>
-©️ <code>@ReNameBot</code>
-Please short your file name and try again!"""
+<b>Esai Tidak diizinkan dalam nama file Telegram!</b>
+©️ <code>@SF_File_Rename_Bot</code>
+Harap persingkat nama file Anda dan coba lagi!
+
+~[@SF Corp](htpps://t.me/SFCorpChannel)"""
