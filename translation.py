56f6069db2b0389a49f7db7469f6a1a651cb8697
rizkysimanjuntaks <91770129+rizkysimanjuntaks@users.noreply.github.com>
2021-10-07 13:50:20 +0700

    Update translation.py

--- translation.py
+++ translation.py
@@ -7,11 +7,59 @@ Ini adalah SF File Rename Bot!
 /help untuk detail lebih lanjut.."""
     RENAME_403_ERR = "Maaf. Anda tidak diizinkan untuk mengganti nama file ini."
     ABS_TEXT = " Tolong jangan egois."
-    UPGRADE_TEXT = "<b>👉 Create own Clone Bot.. </b>  /help for Details"
+    UPGRADE_TEXT = """@AnyDL_Bot Paid Plans
+-------
+Plan: FREE
+Filesize limit: 1500 MB
+Daily limit: UNLIMITED
+Price 🌎: ₹ 0 / 30 Days
+FEATURES:
+👉 <a href="https://t.me/SpEcHlDe/608">All Supported Video Formats of https://rg3.github.io/youtube-dl/supportedsites.html, except HLS videos!</a>
+👉 <a href="https://t.me/SpEcHlDe/660">Get a Telegram sticker as a Telegram downloadable media</a>
+👉 <a href="https://t.me/SpEcHlDe/609">Upload as file from any HTTP link, with custom thumbnail support</a>
+👉 <a href="https://t.me/SpEcHlDe/611">Get Low Speed Direct Download Link of any Telegram file</a>
+-------
+Plan: B
+Filesize limit: 1500 MB
+Daily limit: UNLIMITED
+Price 🇮🇳: ₹ 127 / 30 Days
+<a href="https://p-y.tm/i-h6RdB">Pay Via PayTM</a>
+FEATURES:
+👉 All Above Features +
+👉 <a href="https://telegram.dog/AnyDLBot">Convert To Telegram Audio, the media sent as Telegram Documents</a>
+-------
+Plan: C
+Filesize limit: 1500 MB
+Daily limit: UNLIMITED
+Price 🌎: $ 4.5 / 30 Days
+<a href="https://www.paypal.me/spechide/4.51">Pay via PayPal</a>
+Price 🇮🇳: ₹ 314 / 30 Days
+<a href="https://p-y.tm/ub-p4Um">Pay Via PayTM</a>
+FEATURES:
+👉 All Above Features +
+👉 <a href="https://telegram.dog/AnyDLBot">Convert To Streamable Video, any Telegram file</a>
+👉 <a href="https://t.me/SpEcHlDe/610">ReName Telegram files, with custom thumbnail support</a>
+-------
+Plan: D
+Filesize limit: 1500 MB
+Daily limit: UNLIMITED
+Price 🌎: $ 14.25 / 30 Days
+<a href="https://www.paypal.me/spechide/14.25">Pay via PayPal</a>
+Price 🇮🇳: ₹ 987 / 30 Days
+<a href="https://p-y.tm/WOF-alE">Pay Via PayTM</a>
+FEATURES:
+👉 All Above Features +
+👉 <a href="https://t.me/SpEcHlDe/625">Generate Custom Thumbnail by sending two photos in a Media Album</a>
+👉 <a href="https://t.me/SpEcHlDe/653">Trim large videos</a>, and <a href="https://t.me/SpEcHlDe/652">Take Screenshots</a> of Telegram media files.
+👉 <a href="https://t.me/SpEcHlDe/657">Extract compressed Telegram media</a>
+👉 PLUS, all newly features that are going to be added in the feature*.
+-------
+NOTE: After payment you must take screenshot of receipt and send to @ShriMADhaBot with your Telegram ID obtained from /me
+🙏 Please Message from the ID you want plan to be activated 🙏"""
     DOWNLOAD_START = "Sedang Mengunduh"
     UPLOAD_START = "Sedang Mengunggah"
     RCHD_TG_API_LIMIT = "Diunduh dalam {} detik.\nUkuran file terdeteksi : {}\nMaaf. Namun, saya tidak dapat mengunggah file yang lebih besar dari 1,5 GB karena keterbatasan API Telegram."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Terima kasih telah Menggunakan Saya🤓.\n\n~[@SF Corp](htpps://t.me/SFCorpChannel)**"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Terima kasih telah Menggunakan Saya🤓.\n\n~@SFCorpChannel**"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Diunduh dalam {} detik.\nDiunggah dalam {} detik."
     NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
     NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://telegram.dog/ThankTelegram'>@SpEcHlDe</a>"
@@ -27,7 +75,7 @@ Ini adalah SF File Rename Bot!
 1. Kirimi saya File Telegram apa saja.
 2. Balas file Telegram ke /rename Nama Baru File.extension.
    
-~[@SF Corp](htpps://t.me/SFCorpChannel)
+~@SFCorpChannel
 """
     REPLY_TO_DOC_FOR_RENAME_FILE = "Balas file Telegram ke `/rename Nama Baru File.extension`"
     ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction."
